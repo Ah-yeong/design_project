241e9ca056418ef0a10b81b7fc8d2f046249460d
kimgahyun <mcrkgus@naver.com>
2023-05-31 05:42:58 +0900

    [가현] 채팅 UI 개선

--- lib/Chat/ChatBubble.dart
+++ lib/Chat/ChatBubble.dart
@@ -1,3 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+
+class ChatBubble extends StatelessWidget {
+  const ChatBubble(this.message, this.isMe, {Key? key}) : super(key: key);
+
+  final String message;
+  final bool isMe;
+  //DateTime now = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,  //나 : 왼쪽 / 상대방 : 오른쪽 배치
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: isMe ? Colors.grey[300] : Color(0xFF6ACA89), //나 : grey / 상대방 : blue
+            borderRadius: BorderRadius.only(  //채팅 말풍선? 위치 지정
+                topRight: Radius.circular(12),
+                topLeft: Radius.circular(12),
+                bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+                bottomLeft:  isMe ? Radius.circular(12) : Radius.circular(0)
+            ),
+          ),
+          width: 145,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          child: Text(
+            message,
+            style: TextStyle(
+                color: isMe ? Colors.black : Colors.white //나 : black / 상대방 : white
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
 // import 'package:flutter/material.dart';
 //
 // class ChatBubble extends StatelessWidget {
@@ -35,91 +78,31 @@
 //     );
 //   }
 // }
+
+/////////////////////////////////////////////////////////////////////////
+//채팅 - 말풍선
+// class ChatBubble extends StatelessWidget {
+//   const ChatBubble(this.message, this.isMe, {Key? key}) : super(key: key);
 //
+//   final String message;
+//   final bool isMe;
 //
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+//       children: [
+//         BubbleSpecialOne(
+//           text: message,
+//           color: isMe ? Colors.grey[300]! : Color(0xFF6ACA89),
+//           tail: true,
+//           textStyle: TextStyle(
+//             color: isMe ? Colors.black : Colors.white,
+//           ),
+//           isSender: isMe,
+//         ),
+//       ],
+//     );
+//   }
+// }
 
-
-import 'package:flutter/material.dart';
-
-class BubbleSpecialThree extends StatelessWidget {
-  const BubbleSpecialThree({
-    Key? key,
-    required this.text,
-    required this.color,
-    required this.tail,
-    required this.textStyle,
-  }) : super(key: key);
-
-  final String text;
-  final Color color;
-  final bool tail;
-  final TextStyle textStyle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              width: 5,
-              height: 1,
-              decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(4),
-                  topRight: Radius.circular(4),
-                  bottomLeft: tail ? Radius.circular(0) : Radius.circular(4),
-                  bottomRight: tail ? Radius.circular(0) : Radius.circular(4),
-                ),
-              ),
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(12),
-              topLeft: Radius.circular(12),
-              bottomRight: tail ? Radius.circular(0) : Radius.circular(12),
-              bottomLeft: tail ? Radius.circular(12) : Radius.circular(0),
-            ),
-          ),
-          width: 145,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Text(
-            text,
-            style: textStyle,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ChatBubble extends StatelessWidget {
-  const ChatBubble(this.message, this.isMe, {Key? key}) : super(key: key);
-
-  final String message;
-  final bool isMe;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: [
-        BubbleSpecialThree(
-          text: message,
-          color: isMe ? Colors.grey[300]! : Color(0xFF6ACA89),
-          tail: !isMe,
-          textStyle: TextStyle(
-            color: isMe ? Colors.black : Colors.white,
-          ),
-        ),
-      ],
-    );
-  }
-}
--- lib/Chat/ChatMessage.dart
+++ lib/Chat/ChatMessage.dart
@@ -1,46 +1,13 @@
-// import 'package:flutter/material.dart';
-//
-// const String _name = "Name";
-//
-// class ChatMessage extends StatelessWidget {
-//   ChatMessage({this.text, this.icon});
-//   final String? text;
-//   final Icon? icon;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: const EdgeInsets.symmetric(vertical: 10.0),
-//       child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: <Widget>[
-//           Container(
-//             margin: const EdgeInsets.only(right: 16.0),
-//             child: CircleAvatar(child: Text(_name[0])),
-//           ),
-//           Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: <Widget>[
-//               if (icon!=null) icon!,
-//               Text(_name, style: Theme.of(context).textTheme.bodyText1),
-//               Container(
-//                 margin: const EdgeInsets.only(top: 5.0),
-//                 child: Text(text!),
-//               ),
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
-import 'ChatBubble.dart';
 import 'package:flutter/material.dart';
+import 'ChatBubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+import 'package:intl/intl.dart';
+
+
 
 class Messages extends StatefulWidget {
   const Messages({Key? key}) : super(key: key);
@@ -52,14 +19,11 @@ class Messages extends StatefulWidget {
 class _MessagesState extends State<Messages> {
   late SharedPreferences _preferences;
   final _chatController = TextEditingController();
-
   late DatabaseReference _chatRef;
-  //final _chatController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    //_userEnterMessage = ''; // 초기화 코드를 initState에서 수행
     _chatRef = FirebaseDatabase.instance.reference().child('chat');
     _initSharedPreferences();
   }
@@ -79,7 +43,6 @@ class _MessagesState extends State<Messages> {
       'userID' : user!.uid,
     });
     _chatController.clear();
-
   }
 
   Future<void> _initSharedPreferences() async {
@@ -87,11 +50,6 @@ class _MessagesState extends State<Messages> {
     setState(() {}); // _preferences를 초기화한 후에 다시 build 되도록 setState 호출
   }
 
-  Future<void> _saveChatData(String chatData) async {
-    final savedChatData = _preferences.getStringList('chat_data') ?? [];
-    savedChatData.add(chatData);
-    await _preferences.setStringList('chat_data', savedChatData);
-  }
 
   List<String> _getSavedChatData() {
     return _preferences.getStringList('chat_data') ?? [];
@@ -115,7 +73,6 @@ class _MessagesState extends State<Messages> {
         });
       }
     });
-
 
     return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
       stream: FirebaseFirestore.instance
@@ -148,22 +105,32 @@ class _MessagesState extends State<Messages> {
                   final chat = chatDocs[index].data() as Map<String, dynamic>;
                   final isMe = chat['userID'] == user!.uid;
                   //final userName = chatDocs[index]['userName'];
-                  return ChatBubble(
-                    chat['text'],
-                    isMe,
-                    //userName,
-                  );
-                },
-              ),
+                  final timestamp = chat['time'] as Timestamp;
+                  final dateTime = timestamp.toDate();
+                  final formattedTime = DateFormat.jm().format(dateTime);
+                  //return ChatBubble(chat['text'], isMe,);
+                  return Row(
+                      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                      children: [
+                        ChatBubble(chat['text'], isMe),
+                        Text(
+                          formattedTime,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
             ),
             Container(
               width: 382,
               height: 40,
               child: TextField(
                 controller: _chatController,
-                onSubmitted: (value) {
-                  _sendMessage();
-                },
+                onSubmitted: (value) {_sendMessage();},
                 decoration: InputDecoration(
                   hintText: 'Enter your message',
                   border: OutlineInputBorder(
@@ -171,9 +138,7 @@ class _MessagesState extends State<Messages> {
                   ),
                   contentPadding: EdgeInsets.symmetric(vertical: 12.0),
                   suffixIcon: IconButton(
-                    onPressed: () {
-                      _sendMessage();
-                    },
+                    onPressed: () {_sendMessage();},
                     icon: Icon(Icons.send),
                   ),
                 ),
@@ -185,3 +150,182 @@ class _MessagesState extends State<Messages> {
     );
   }
 }
+
+//
+// import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:firebase_database/firebase_database.dart';
+//
+// class Messages extends StatefulWidget {
+//   const Messages({Key? key}) : super(key: key);
+//
+//   @override
+//   _MessagesState createState() => _MessagesState();
+// }
+//
+// class _MessagesState extends State<Messages> {
+//   late SharedPreferences _preferences;
+//   final _chatController = TextEditingController();
+//   late DatabaseReference _chatRef;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _chatRef = FirebaseDatabase.instance.reference().child('chat');
+//     _initSharedPreferences();
+//   }
+//
+//   @override
+//   void dispose() {
+//     _chatController.dispose();
+//     super.dispose();
+//   }
+//
+//   void _sendMessage() {
+//     final user = FirebaseAuth.instance.currentUser;
+//     final chatData = _chatController.text.trim();
+//     FirebaseFirestore.instance.collection('chat').add({
+//       'text': chatData,
+//       'time': Timestamp.now(),
+//       'userID': user!.uid,
+//     });
+//     _chatController.clear();
+//   }
+//
+//   Future<void> _initSharedPreferences() async {
+//     _preferences = await SharedPreferences.getInstance();
+//     setState(() {}); // _preferences를 초기화한 후에 다시 build 되도록 setState 호출
+//   }
+//
+//   List<String> _getSavedChatData() {
+//     return _preferences.getStringList('chat_data') ?? [];
+//   }
+//
+//   DatabaseReference chatRef = FirebaseDatabase.instance.reference().child('chat');
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final user = FirebaseAuth.instance.currentUser;
+//     final savedChatData = _getSavedChatData();
+//
+//     _chatRef.onChildAdded.listen((event) {
+//       final chatData = event.snapshot.value as Map<dynamic, dynamic>;
+//       final isMe = chatData['userID'] == user!.uid;
+//
+//       // 상대방의 화면에 채팅 메시지 표시
+//       if (!isMe) {
+//         setState(() {
+//           savedChatData.add(chatData['text']);
+//         });
+//       }
+//     });
+//
+//     return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+//         stream: FirebaseFirestore.instance
+//         .collection('chat')
+//         .orderBy('time', descending: true)
+//         .snapshots(),
+//     builder: (context, snapshot) {
+//       if (snapshot.connectionState == ConnectionState.waiting) {
+//         return Center(
+//           child: CircularProgressIndicator(),
+//         );
+//       }
+//       final chatDocs = snapshot.data!.docs;
+//       void handleData(QuerySnapshot<Map<String, dynamic>> snapshot) {
+//       final chatDocs = snapshot.docs;
+//       // 변경된 데이터 가져오기
+//       final newData = chatDocs.map((doc) => doc.data()).toList();
+//       print('New Data: $newData');
+//       }
+//       // 초기 데이터 처리
+//       handleData(snapshot.data!);
+//       return Column(
+//         children: [
+//           Expanded(
+//             child: ListView.builder(
+//               reverse: true,
+//               itemCount: chatDocs.length,
+//               itemBuilder: (context, index) {
+//               final chat = chatDocs[index].data() as Map<String, dynamic>;
+//               final isMe = chat['userID'] == user!.uid;
+//               final timestamp = chat['time'] as Timestamp;
+//               final dateTime = timestamp.toDate();
+//               final formattedTime = DateFormat.jm().format(dateTime);
+//
+//             return Row(
+//               mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+//               children: [
+//                 ChatBubble(chat['text'], isMe),
+//                 Text(
+//                   formattedTime,
+//                   style: TextStyle(
+//                     fontSize: 12,
+//                     color: Colors.grey,
+//                   ),
+//                 ),
+//               ],
+//             );
+//               },
+//             ),
+//           ),
+//           Container(
+//             width: 382,
+//             height: 40,
+//             child: TextField(
+//               controller: _chatController,
+//               onSubmitted: (value) {
+//                 _sendMessage();
+//               },
+//               decoration: InputDecoration(
+//                 hintText: 'Enter your message',
+//                 border: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(9.0),
+//                 ),
+//                 contentPadding: EdgeInsets.symmetric(vertical: 12.0),
+//                 suffixIcon: IconButton(
+//                   onPressed: () {
+//                     _sendMessage();
+//                   },
+//                   icon: Icon(Icons.send),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       );
+//     },
+//     );
+//   }
+// }
+//
+// class ChatBubble extends StatelessWidget {
+//   final String text;
+//   final bool isMe;
+//
+//   ChatBubble(this.text, this.isMe);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+//       padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+//       decoration: BoxDecoration(
+//         color: isMe ? Colors.blueAccent : Colors.grey[200],
+//         borderRadius: BorderRadius.circular(16),
+//       ),
+//       child: Text(
+//         text,
+//         style: TextStyle(
+//           fontSize: 16,
+//           color: isMe ? Colors.white : Colors.black,
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+//}
