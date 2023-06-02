508ae1e062f3db8577040797b4f3fa68c776c4b0
jongwone <gitlimjw@gmail.com>
2023-06-02 16:59:25 +0900

    [종원] 포스트, gps 관련 수정 - 포스트 로드 최적화 - 애뮬레이터 GPS 상명대학교 초기화

--- lib/Entity/EntityPostPageManager.dart
+++ lib/Entity/EntityPostPageManager.dart
@@ -1,6 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get/get.dart';
-
 import 'EntityPost.dart';
 
 class PostPageManager {
@@ -30,40 +28,28 @@ class PostPageManager {
   }
 
   Future<void> loadPages() async {
-    int post_count = 0;
-    DocumentReference<Map<String, dynamic>> ref =
-        await FirebaseFirestore.instance.collection("Post").doc("postData");
-    await ref.get().then((DocumentSnapshot ds) {
-      post_count = ds.get("post_count");
-    });
     var qs = await FirebaseFirestore.instance.collection("Post").get();
+    // FirebaseFirestore.instance.collection("Post").orderBy("post_id", descending: false).get(); => 정렬로 가져오기
+    // descending : false 시 post_id에 대하여 내림차순 정렬 로드
+
     for(DocumentSnapshot ds in qs.docs) {
-      if(post_count <= _loadedCount || _maxCount <= _loadedCount) {
+      // 한 번에 로드될 게시물의 개수 넘어가면 로드 정지
+      if(_maxCount <= _loadedCount) {
         isLoading = false;
         return;
       }
+
+      // postData값 건너 뜀
       if (ds.id == "postData") continue;
-      if(int.parse(ds.id) <= _lastLoaded) continue;
-      EntityPost pg = EntityPost(int.parse(ds.id));
-      await pg.loadPost();
-      list.add(pg);
+
+      EntityPost post = EntityPost(int.parse(ds.id));
+      await post.loadPost();
+      list.add(post);
       _loadedCount++;
       _lastLoaded = int.parse(ds.id);
     }
     isLoading = false;
     return;
-    // forEach((doc) async {
-    //   if(doc.id != "postData" && _maxCount > _loadedCount && int.parse(doc.id) > _lastLoaded) {
-    //     if(int.parse(doc.id) > _lastLoaded) {
-    //       EntityPostGroup pg = EntityPostGroup(doc.id);
-    //       await pg.loadPost();
-    //       list.add(pg);
-    //       _loadedCount++;
-    //       _lastLoaded = int.parse(doc.id);
-    //       print("로드완료, $_loadedCount");
-    //     }
-    //   }
-    // });
   }
 
   int get maxCount => _maxCount;
