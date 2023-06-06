95d8c9b34d7ce1e0078cd88a95a8f27771623afe
ahyeong <Ah-yeong>
2023-06-07 01:18:24 +0900

    [아영] 프로필에 postid 리스트(내가 작성한 글) 출력

--- lib/Profiles/PageProfile.dart
+++ lib/Profiles/PageProfile.dart
@@ -15,7 +15,7 @@ class PageProfile extends StatefulWidget {
 
 class _PageProfileState extends State<PageProfile> {
   EntityProfiles? myProfile;
-  EntityPost? myPost;
+  List<EntityPost> myPostList = List.empty(growable: true);
   MannerTemperatureWidget? mannerWidget;
 
   @override
@@ -115,55 +115,6 @@ class _PageProfileState extends State<PageProfile> {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(
-                          ' 내가 속한 모임',
-                          style: TextStyle(
-                            fontSize: 16,
-                            //fontWeight: FontWeight.bold,
-                          )
-                      ),
-                      SizedBox(height: 10),
-                      // Row(
-                      //   children: [
-                      //     Card(
-                      //       child: Padding(
-                      //         padding: const EdgeInsets.all(7),
-                      //         child: _buildFriendRow(myPost!),
-                      //       ),
-                      //     ),
-                      //     Container(
-                      //
-                      //     )
-                      //   ],
-                      // )
-                      Card(
-                        child: GestureDetector(
-                            onTap: (){
-                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => BoardPostPage(postId: myPost!.getPostId())));
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(7),
-                              child: _buildFriendRow(myPost!),
-                            )
-                        )
-                      ),
-                    ]
-                ),
-              ),
-              Divider(thickness: 1, height: 1),
-              Container(
-                padding: EdgeInsets.all(15),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // Text(
-                      //     '내 정보',
-                      //     style: TextStyle(
-                      //       fontSize: 16,
-                      //       fontWeight: FontWeight.bold,
-                      //     )
-                      // ),
-                      // SizedBox(height: 10),
                       Container(
                         child: Column(
                           children: [
@@ -243,104 +194,48 @@ class _PageProfileState extends State<PageProfile> {
                                   )
                                 ]
                             ),
-                            SizedBox(
-                              height: 7,
-                            ),
-                            Divider(thickness: 1, height: 1),
-                            SizedBox(
-                              height: 7,
-                            ),
+                            // Divider(thickness: 1, height: 1),
                           ],
                         ),
-                      //     padding:  EdgeInsets.all(15),
-                      //     decoration: BoxDecoration(
-                      //       borderRadius: BorderRadius.circular(10),
-                      //       border: Border.all(
-                      //         width: 2,
-                      //         color: Color(0xFF6ACA89),
-                      //       ),
-                      //     ),
-                      //     child: Row(
-                      //         crossAxisAlignment: CrossAxisAlignment.center,
-                      //         children: <Widget>[
-                      //     Column(
-                      //     crossAxisAlignment: CrossAxisAlignment.start,
-                      //       children: <Widget>[
-                      //         Text(
-                      //           '취미',
-                      //           style: TextStyle(
-                      //             fontSize: 14,
-                      //           ),
-                      //
-                      //         ),
-                      //         SizedBox(height: 6),
-                      //         Text(
-                      //           'MBTI',
-                      //           style: TextStyle(
-                      //             fontSize: 14,
-                      //           ),
-                      //         ),
-                      //         SizedBox(height: 6),
-                      //         Text(
-                      //           '통학여부',
-                      //           style: TextStyle(
-                      //             fontSize: 14,
-                      //           ),
-                      //         ),
-                      //         SizedBox(height: 6),
-                      //         Text(
-                      //           '거주지',
-                      //           style: TextStyle(
-                      //             fontSize: 14,
-                      //           ),
-                      //         ),
-                      //       ],
-                      //     ),
-                      //     SizedBox(width: 20),
-                      //     Expanded(
-                      //       child: Column(
-                      //         crossAxisAlignment: CrossAxisAlignment.start,
-                      //         children: <Widget>[
-                      //           Text(
-                      //             '${myProfile!.hobby?.join(', ')}',
-                      //             // '단대호수 산책',
-                      //             style: TextStyle(
-                      //               fontSize: 14,
-                      //             ),
-                      //             maxLines: 2, // 텍스트가 2줄을 초과하면 다음 줄로 내려가도록 설정
-                      //             overflow: TextOverflow.ellipsis, // 텍스트가 오버플로우되는 경우 ...으로 표시
-                      //           ),
-                      //           SizedBox(height: 6),
-                      //           Text(
-                      //             "${myProfile!.mbti}",
-                      //             style: TextStyle(
-                      //               fontSize: 14,
-                      //             ),
-                      //           ),
-                      //           SizedBox(height: 6),
-                      //           Text(
-                      //             "${myProfile!.commute}",
-                      //             style: TextStyle(
-                      //               fontSize: 14,
-                      //             ),
-                      //           ),
-                      //           SizedBox(height: 6),
-                      //           Text(
-                      //             '경기도 오산시',
-                      //             style: TextStyle(
-                      //               fontSize: 14,
-                      //             ),
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     ),
-                      //         ]
-                      //     )
                       )
                     ]
                 ),
-              )
+              ),
               // 추가적인 프로필 정보를 이곳에 추가할 수 있습니다.
+              Divider(thickness: 1, height: 1),
+              Container(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '내가 작성한 글',
+                      style: TextStyle(
+                        fontSize: 16,
+                        //fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Column(
+                      children: myPostList.map((post) {
+                        return Card(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => BoardPostPage(postId: post.getPostId()),
+                              ));
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(7),
+                              child: _buildFriendRow(post),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                )
+              ),
             ],
           ),
         ),
@@ -354,12 +249,14 @@ class _PageProfileState extends State<PageProfile> {
     super.initState();
     myProfile = EntityProfiles(FirebaseAuth.instance.currentUser!.uid);
     myProfile!.loadProfile().then((n) {
-      myPost = EntityPost(myProfile!.post);
       mannerWidget = MannerTemperatureWidget(mannerScore: myProfile!.mannerGroup);
-      myPost!.loadPost().then((value) {
-        setState(() {});
+      for( var postId in myProfile!.post){
+        EntityPost myPost = EntityPost(postId);
+        myPost.loadPost().then((value) {
+          myPostList.add(myPost);
+          setState(() {});
+        });
       }
-      );
     });
   }
 
