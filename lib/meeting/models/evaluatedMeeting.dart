c0ed097747b3dfb24ebd2505cf192ed6a89de52f
ahyeong <Ah-yeong>
2023-10-26 03:38:13 +0900

    [아영] endMeeting(EvaluatedMeetings) - 평가 가능 기한 표시, 그에 따른 버튼 표시

---
+++ lib/meeting/models/evaluatedMeeting.dart
@@ -0,0 +1,109 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:design_project/entity/latlng.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+import '../../chat/chat_screen.dart';
+import '../../resources/resources.dart';
+import '../../boards/post_list/post_list.dart';
+import '../meeting_evaluate.dart';
+
+class EvaluatedMeeting {
+  int _meetingId;
+  DateTime _meetTime;
+  List<String> _members;
+  String _address;
+  bool _isVoluntary;
+  Map<String, dynamic> _arrivals;
+
+  CollectionReference _meetingInstance = FirebaseFirestore.instance.collection("Meetings");
+  CollectionReference _userInstance = FirebaseFirestore.instance.collection("UserMeetings");
+
+  EvaluatedMeeting(this._address, this._arrivals, this._isVoluntary, this._meetTime, this._meetingId, this._members);
+
+  getMeetTime() {return this._meetTime;}
+
+  DocumentReference getMeetingDocument() {
+    return _meetingInstance.doc(_meetingId.toString());
+  }
+
+  bool isOverDeadline(String date){
+    DateTime deadline = DateFormat('yyyy-MM-dd').parse(date).add(Duration(days: 3));
+    DateTime currentDate = DateTime.now();
+    return currentDate.isBefore(deadline) || currentDate.isAtSameMomentAs(deadline);
+  }
+
+  Widget buildEndMeetingCard() {
+    String timeText = getMeetTimeText(_meetTime.toString());
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Row(children: [
+                      SizedBox(width: 35, child: Icon(CupertinoIcons.person_3_fill)),
+                      Text(
+                        '$timeText 모임', // 글 제목
+                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.start,
+                      ),
+                    ]),
+                  ]),
+                const Padding(padding: EdgeInsets.only(bottom: 5)),
+                Row(
+                  children: [
+                    const Icon(Icons.location_on_outlined, color: colorGrey, size: 13),
+                    Text(" ${_address}", style: TextStyle(fontSize: 11, color: Color(0xFF858585))),
+                    SizedBox(height: 1),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    const Icon(Icons.emoji_people, color: colorGrey, size: 13),
+                    Text(_isVoluntary ? " 자율 참여" : " 위치 공유",
+                        style: TextStyle(fontSize: 11, color: Color(0xFF858585))),
+                    SizedBox(height: 1),
+                  ],
+                ),
+                SizedBox(height: 5),
+              ],
+            ),
+            isOverDeadline(DateFormat('yyyy-MM-dd').format(_meetTime)) ?
+            Container(
+              decoration: BoxDecoration(color: Colors.indigoAccent, borderRadius: BorderRadius.circular(10)),
+              width: 90,
+              height: 35,
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(8),
+                  overlayColor: MaterialStateProperty.all(Colors.white38),
+                  onTap: () {
+                    Get.off(() => PageMeetingEvaluate(
+                      members: _members,
+                    ));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("평가하기 ", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 13)),
+                      Icon(Icons.arrow_forward_ios, color: Colors.white, size: 15,),
+                    ],
+                  ),
+                ),
+              ),
+            ) : Container(),
+          ],
+        ),
+        Divider(thickness: 1,)
+      ],
+    );
+  }
+}
--- lib/profiles/completed_group.dart
+++ lib/profiles/completed_group.dart
@@ -1,12 +1,14 @@
+import 'package:design_project/meeting/meeting_evaluate.dart';
+import 'package:design_project/meeting/models/Evaluated_meeting_manager.dart';
+import 'package:design_project/meeting/models/evaluation_manager.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../boards/post.dart';
-import '../entity/entity_post.dart';
+import 'package:intl/intl.dart';
 import '../entity/profile.dart';
-
+import '../meeting/models/evaluatedMeeting.dart';
 import '../resources/loading_indicator.dart';
 import '../resources/resources.dart';
-import '../boards/post_list/post_list.dart';
 
 class PageMyEndGroup extends StatefulWidget {
   @override
@@ -15,7 +17,9 @@ class PageMyEndGroup extends StatefulWidget {
 
 class _PageMyEndGroup extends State<PageMyEndGroup> {
   EntityProfiles? myProfile;
-  List<EntityPost> myEndGroupList = List.empty(growable: true);
+  List<EvaluatedMeeting> myEndMeetingList = [];
+  Map<String, List<EvaluatedMeeting>> _groupedMeetings = {};
+  bool _isLoadingPost = false;
 
   @override
   Widget build(BuildContext context) {
@@ -41,139 +45,131 @@ class _PageMyEndGroup extends State<PageMyEndGroup> {
         elevation: 1,
       ),
       backgroundColor: Colors.white,
-      body: myProfile!.isLoading ? buildLoadingProgress():
-      SingleChildScrollView(
-        padding: EdgeInsets.all(10),
-        child: myEndGroupList.isEmpty ?
-        Column(
-          children: [
-            SizedBox(height: 20),
-            Center(
-                child: Text("종료된 모임이 아직 없습니다.",
-                    style: TextStyle(color: Colors.grey, fontSize: 15)
-                )
-            )
-          ],
-        ) :
-        Column(
-          children: myEndGroupList.map((group) {
-            return Card(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => BoardPostPage(postId: group.getPostId()),
-                  ));
+        body: !_isLoadingPost ? buildLoadingProgress()
+          : myEndMeetingList.length != 0 ?
+            SingleChildScrollView(
+              padding: EdgeInsets.all(10),
+              child: ListView.separated(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: _groupedMeetings.length,
+                separatorBuilder: (context, index) => Divider(),
+                itemBuilder: (context, index) {
+                  final dateKey = _groupedMeetings.keys.elementAt(index);
+                  final meetings = _groupedMeetings[dateKey];
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(width: 3),
+                          Text(
+                            dateKey,
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      isOverDeadline(DateAddThreeDays(dateKey)) ?
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(7, 10, 7, 7),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 6.0),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: const Color(0xFFEAEAEA),
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Center(
+                              child: Text("${DateAddThreeDays(dateKey)} 까지 평가 가능")),
+                        ),
+                      ) : Container(),
+                      Column(
+                        children: meetings!.map((meeting) {
+                          return Container(
+                            child: GestureDetector(
+                              onTap: () {
+                                // 모임 채팅방으로 이동해야 되나 ?
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(7, 7, 7, 0),
+                                child: meeting.buildEndMeetingCard(),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  );
                 },
-                child: Padding(
-                  padding: const EdgeInsets.all(7),
-                  child: _buildFriendRow(group),
+              )
+            )
+            : Center (
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("아직 종료된 모임이 없습니다.",
+                      style: TextStyle(color: colorGrey),)
+                  ],
                 ),
-              ),
-            );
-          }).toList(),
-        ),
-      ),
+              )
     );
   }
 
   @override
   void initState() {
     super.initState();
-    myProfile = EntityProfiles(FirebaseAuth.instance.currentUser!.uid);
-    myProfile!.loadProfile().then((n) {
-      // if(myProfile!.endGroup.isEmpty){
-      //   setState(() {});
-      // } else {
-      // for (var groupId in myProfile!.endGroup) {
-      //   EntityPost myEndGroup = EntityPost(groupId);
-      //   myEndGroup.loadPost().then((value) {
-      //     myEndGroupList.add(myEndGroup);
-      //     setState(() {});
-      //   });
-      // }}
+    var evalMeetingManager = EvaluatedMeetingManager();
+    evalMeetingManager.getUserEndMeetingData(FirebaseAuth.instance.currentUser!.uid).then((meetingList) async {
+      Future.forEach(meetingList, (meetingId) async {
+        await evalMeetingManager.getEvaluatedMeeting(meetingId).then((meeting) {
+          if (meeting != null) myEndMeetingList.add(meeting);
+          if (meetingList.length == myEndMeetingList.length) {
+            myEndMeetingList.sort((a, b) =>
+                a.getMeetTime().compareTo(b.getMeetTime()));
+            Map<String, List<EvaluatedMeeting>> evaluableMeetings = {};
+            Map<String, List<EvaluatedMeeting>> notEvaluableMeetings = {};
+
+            for (var meeting in myEndMeetingList) {
+              final dateKey = DateFormat('yyyy-MM-dd').format(
+                  meeting.getMeetTime());
+              if (isOverDeadline(DateAddThreeDays(dateKey))){
+                if (!evaluableMeetings.containsKey(dateKey)) {
+                  evaluableMeetings[dateKey] = [];
+                }
+                evaluableMeetings[dateKey]!.add(meeting);
+              }
+              else{
+                if (!notEvaluableMeetings.containsKey(dateKey)) {
+                  notEvaluableMeetings[dateKey] = [];
+                }
+                notEvaluableMeetings[dateKey]!.add(meeting);
+              }
+            }
+            evaluableMeetings.addAll(notEvaluableMeetings);
+            setState(() => _groupedMeetings = evaluableMeetings);
+          }
+        });
+      }).then((value) => setState(() => _isLoadingPost = true));
     });
   }
 
-  Widget _buildFriendRow(EntityPost entity) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(
-          width: 5,
-        ),
-        Flexible(
-          fit: FlexFit.tight,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.only(top: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${entity!.getPostHead()}', // 글 제목
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.start,
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(right: 5, bottom: 5),
-                          child: Container(
-                            width: 60,
-                            height: 25,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              color: const Color(0xFF6ACA9A),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: Center(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Icon(Icons.emoji_people, color: Colors.white, size: 14,),
-                                      Text(getMaxPersonText(entity),
-                                          style: const TextStyle(
-                                              color: Colors.white, fontSize: 10)),
-                                    ],
-                                  )
-                              ),
-                            ),
-                          )
-                      )
-                    ],
-                  )
-              ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 5),
-              ),
-              Row(
-                children: [
-                  const Icon(Icons.location_on_outlined, color: colorGrey, size: 13,),
-                  Text(
-                    " ${entity.getLLName().AddressName}",
-                    style: TextStyle(fontSize: 11, color: Color(0xFF858585)),
-                  ),
-                  SizedBox(height: 1,),
-                ],
-              ),
-              SizedBox(height: 1,),
-              Row(
-                children: [
-                  const Icon(Icons.timer_outlined, color: colorGrey, size: 13,),
-                  Text(
-                    getMeetTimeText(entity.getTime()),
-                    style: TextStyle(fontSize: 11, color: Color(0xFF858585)),
-                  ),
-                ],
-              ),
-              SizedBox(height: 5,),
-            ],
-          ),
-        ),
-      ],
-    );
+  String DateAddThreeDays(String datakey) {
+    DateTime date = DateFormat('yyyy-MM-dd').parse(datakey);
+    DateTime addThreeDays = date.add(Duration(days: 3));
+    String newDate = DateFormat('yyyy-MM-dd').format(addThreeDays);
+    return newDate;
   }
+
+  bool isOverDeadline(String date){
+    DateTime deadline = DateFormat('yyyy-MM-dd').parse(date);
+    DateTime currentDate = DateTime.now();
+    // print("${deadline}, ${currentDate}");
+    return currentDate.isBefore(deadline) || currentDate.isAtSameMomentAs(deadline);
+  }
+
 }
